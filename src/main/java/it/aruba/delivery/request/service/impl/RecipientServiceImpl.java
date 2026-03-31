760548d3e27997bc8d723fee4f1fdad6cfed3a32
lmarchettini <lmarchettini1986@gmail.com>
2026-03-31 17:44:41 +0200

    refactor

--- src/main/java/it/aruba/delivery/request/service/impl/RecipientServiceImpl.java
+++ src/main/java/it/aruba/delivery/request/service/impl/RecipientServiceImpl.java
@@ -1,9 +1,10 @@
-package it.aruba.delivery.request.service;
+package it.aruba.delivery.request.service.impl;
 
 import org.springframework.stereotype.Service;
 
 import it.aruba.delivery.request.dto.RecipientResponse;
 import it.aruba.delivery.request.http.RecipientClient;
+import it.aruba.delivery.request.service.RecipientService;
 import lombok.RequiredArgsConstructor;
 
 @Service
